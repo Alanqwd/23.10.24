1a12bba94075ece871a76d36242c5cc232f7f781
Alanqwd <nurmatovnurbek555@gmail.com>
2024-10-25 18:30:10 +0500

    a

--- 23.10.24/Hand.hpp
+++ 23.10.24/Hand.hpp
@@ -1,1 +1,17 @@
 #pragma once
+#ifndef HAND_HPP
+#define HAND_HPP
+#include <vector>
+#include "Card.hpp"
+
+namespace CardGame {
+    
+	class Hand {
+	public:
+
+	private:
+		std::vector<Card> cards_;
+	};
+}
+
+#endif
