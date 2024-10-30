d68f23df3984534cbf908e4f398ca9432278ed29
Alanqwd <nurmatovnurbek555@gmail.com>
2024-10-30 17:26:19 +0500

    efe

--- 23.10.24/Deck.hpp
+++ 23.10.24/Deck.hpp
@@ -1,3 +1,17 @@
 #pragma once
+#ifndef HAND_HPP
+#define HAND_HPP
+#include <vector>
+#include "Card.hpp"
 
-class Deck;
+namespace CardGame {
+
+    class Deck {
+    public:
+        Deck() = default;
+        Deck(const std::vector<CardGame::Deck>& cards) : cards_(cards) {}
+    private:
+        std::vector<CardGame::Deck> cards_;
+    };
+}
+#endif 
--- 23.10.24/Hand.hpp
+++ 23.10.24/Hand.hpp
@@ -5,18 +5,22 @@
 #include "Card.hpp"
 
 namespace CardGame {
-	enum Kit {
-			card
-		};
-	class Hand {
-	public:
-			Hand() = default;
-			Hand(Card cards): cards_(cards)
-			{}
-			Hand GetCard()const;
-		private:
-			std::vector<Card> cards_;
 
-		};
-	};
+    class Hand {
+    public:
+        Hand() = default;
+        Hand(const std::vector<CardGame::Card>& cards) : cards_(cards) {} 
+
+        CardGame::Card Show(int num) const;
+        CardGame::Card GetCard(int num);
+
+        void PlaceCard(const CardGame::Card& new_card);
+
+        int size() const { return cards_.size(); }
+
+        const std::vector<CardGame::Card>& getCards() const { return cards_; }
+    private:
+        std::vector<CardGame::Card> cards_;
+    };
+}
 #endif 
